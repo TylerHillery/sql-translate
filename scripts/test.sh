288278fc832c97233e148a8997d04ef3824ef97e
Tyler Hillery <tyhillery@gmail.com>
2025-02-16 08:20:33 -0700

    chore: starting over

--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-coverage run --source=sql_translate --omit=sql_translate/main.py -m pytest
-coverage report --show-missing
-coverage html --title "${@-coverage}"
--- scripts/update_requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-uv pip compile requirements.in -o requirements.txt
-uv pip compile requirements-dev.in -o requirements-dev.txt
-uv pip sync requirements-dev.txt
